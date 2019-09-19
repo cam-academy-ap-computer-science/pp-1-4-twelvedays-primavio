137c8b14af3961d784f58f05205f03ddae516a91
Violetta <Violetta@192.168.0.5>
2019-09-18 23:23:43 -0700

    i sure hope this works because its midnight and im tired :) if it doesnt im really not trying again

--- src/TwelveDays.java
+++ src/TwelveDays.java
@@ -2,8 +2,152 @@
 public class TwelveDays {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		first();
+		mtl();
+		ppt();
+		second();
+		ttd();
+		third();
+		tfh();
+		fourth();
+		fcb();
+		fifth();
+		fgr();
+		sixth();
+		sgl();
+		seventh();
+		ssw();
+		eighth();
+		emm();
+		ninth();
+		nld();
+		tenth();
+		tll();
+		eleventh();
+		epp();
+		twelfth();
+		tdd();
 	}
-
+	
+	public static void first() {
+		System.out.println("On the first day of Christmas,");
+	}
+	
+	public static void mtl() {
+		System.out.println("my true love sent to me");
+	}
+	
+	public static void ppt() {
+		System.out.println("a partrige in a pear tree.");
+		System.out.println("");
+	}
+ 
+	public static void second() {
+		System.out.println("On the second day of Christmas,");
+		mtl();
+	}
+	
+	public static void ttd() {
+		System.out.println("two turtle doves, and");
+		ppt();
+	}
+	
+	public static void third() {
+		System.out.println("On the third day of Christmas,");
+		mtl();
+	}
+	
+	public static void tfh() {
+		System.out.println("three french horns,");
+		ttd();
+	}
+	
+	public static void fourth() {
+		System.out.println("On the fourth day of Christmas,");
+		mtl();
+    }
+	
+	public static void fcb() {
+		System.out.println("four calling birds,");
+		tfh();
+	}
+	
+	public static void fifth() {
+		System.out.println("On the fifth day of Christmas,");
+		mtl();
+	}
+	
+	public static void fgr() {
+		System.out.println("five golden rings,");
+		fcb();
+	}
+	
+	public static void sixth() {
+		System.out.println("On the fourht day of Christmas,");
+		mtl();
+	}
+	
+	public static void sgl() {
+		System.out.println("six geese a-laying,");
+		fgr();
+	}
+	
+	public static void seventh() {
+		System.out.println("On the seventh day of Christmas,");
+		mtl();
+	}
+	
+	public static void ssw() {
+		System.out.println("seven swans a-swimming,");
+		sgl();
+	}
+	
+	public static void eighth() {
+		System.out.println("On the eighth day of Christmas,");
+		mtl();
+	}
+	
+	public static void emm() {
+		System.out.println("eight maids a-milking,");
+		ssw();
+	}
+	
+	public static void ninth() {
+		System.out.println("On the ninth day of Christmas,");
+		mtl();
+	}
+	
+	public static void nld() {
+		System.out.println("nine ladies dancing,");
+		emm();
+	}
+	public static void tenth() {
+		System.out.println("On the tenth day of Christmas,");
+		mtl();
+	}
+		
+	public static void tll() {
+		System.out.println("ten lords a-leaping");
+		nld();
+	}
+	
+	public static void eleventh() {
+		System.out.println("On the eleventh day of Christmas,");
+		mtl();
+	}
+	
+	public static void epp() {
+		System.out.println("eleven pipers piping");
+		tll();
+	}
+	
+	public static void twelfth() {
+		System.out.println("On the twelfth day of Christmas,");
+		mtl();
+	}
+	
+	public static void tdd() {
+		System.out.println("Twelve drummers drumming");
+		epp();
+	}
 }
